39b778cfde3b4c5c35b518366a22e7dc7393d0fa
Jonas Claes <jonas@jonasclaes.be>
2023-01-28 14:47:25 +0100

    Added support for Pocketbase external server

--- app/src/lib/pocketbase.ts
+++ app/src/lib/pocketbase.ts
@@ -1,7 +1,8 @@
+import { env } from '$env/dynamic/public';
 import PocketBase from 'pocketbase';
 import { writable } from 'svelte/store';
 
-export const pb = new PocketBase('http://127.0.0.1:8090');
+export const pb = new PocketBase(env.PUBLIC_POCKETBASE_URL);
 
 export const currentUser = writable(pb.authStore.model);
 
